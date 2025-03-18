2ea7b5d5cd06b1d29156ce23683937ad6a9a60ce
eayzex <exeayzex@gmail.com>
2025-03-18 22:18:34 +0100

    Add new files

---
+++ week2/Day2/ex1.sql
@@ -0,0 +1,43 @@
+-- select * from customer;
+-- select concat(first_name, ' ', last_name) AS full_name from customer;
+-- select distinct create_date from customer;
+-- select * from customer 
+-- order by first_name desc;
+-- select * from film ;
+-- select film_id, title, description, release_year, rental_rate 
+-- from film 
+-- order by rental_rate asc;
+-- select address , phone from address where district = 'Texas'
+-- select * from film 
+-- where film_id in (15, 150);
+-- select * from film ;
+-- select film_id, title, description, length, rental_rate 
+-- from film 
+-- where title = 'Alien Center'
+-- select * from film ; 
+-- select film_id, title, description, length, rental_rate
+-- from film 
+-- where title like  'Fi%';
+-- select * from film 
+-- order by rental_rate asc limit 10; 
+-- select * from film ;
+-- SELECT film_id, title, rental_rate
+-- FROM film
+-- ORDER BY rental_rate ASC
+-- OFFSET 10 ROWS FETCH FIRST 10 ROWS ONLY;
+-- select * from film ;
+-- select customer.first_name, customer.last_name, payment.amount, payment.payment_date 
+-- from customer 
+-- inner join payment on customer.customer_id = payment.customer_id 
+-- order by customer.customer_id asc; 
+-- SELECT * FROM film ;  
+-- SELECT * 
+-- FROM film 
+-- WHERE film_id NOT IN (SELECT DISTINCT film_id FROM inventory);
+-- select city.city, country.country 
+-- from city 
+-- inner join country on city.country_id = country.country_id;
+-- select customer.customer_id, customer.first_name, customer.last_name 
+-- from customer 
+-- INNER JOIN payment ON customer.customer_id = payment.customer_id 
+-- order by payment.staff_id asc ; 
