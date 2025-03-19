caf6aa89917b3763a28bc288dcb5196361463ba5
eayzex <exeayzex@gmail.com>
2025-03-19 10:07:16 +0100

    Add new files

---
+++ week2/Day2/challenge.sql
@@ -0,0 +1,23 @@
+    -- SELECT COUNT(*) 
+    -- FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+reponse = 0 
+
+-- SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+
+reponse = 2
+
+
+    -- SELECT COUNT(*) 
+    -- FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+ reponse = 0 
+
+    --  SELECT COUNT(*) 
+    -- FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+
+reponse = 2 
+
+
+
+
+
